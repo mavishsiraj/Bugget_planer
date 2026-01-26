272557d29144f7122f82d8d6df77258aa4f66a8e
Taiba Siraj <mavishsiraj1@gmail.com>
2026-01-27 03:18:09 +0530

    db connection added and authentication added

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,17 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 
 dotenv.config();
-
 const app = express();
+const connectDB = require("./config/db");
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
-  res.send("Budget Planner Backend is Running");
+  res.send("Budget Planner API Running ");
 });
 
 const PORT = process.env.PORT || 5000;
